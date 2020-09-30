06c1e431821c244e81a4ec049a750fc4b43bf0f3
SamuelFischerCode <65810622+SamuelFischerCode@users.noreply.github.com>
2020-09-30 20:56:21 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to install
+
+* Run `curl install.sh -o install.sh; ./install.sh; rm -rf install.sh;`
+* Boom
---
+++ install.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+curl https://expresshost.github.io/neh -o neh; cp neh /usr/local/bin; rm /usr/local/bin;
